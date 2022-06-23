bcd86b80f7bc355a8edee25f95ac574e442aaa89
Vadim-Maul <v.v.maul681@gmail.com>
2022-06-23 20:40:24 +0200

    add printHelp

--- services/log.service.js
+++ services/log.service.js
@@ -1,11 +1,23 @@
-import { bgRed, bgGreen } from "chalk";
+import chalk from "chalk";
+import dedent from "dedent-js";
 
 const printError = (error) => {
-  console.log(bgRed(" ERROR ") + " " + error);
+  console.log(`${chalk.bgRed(" ERROR ")}  ${error}`);
 };
 
 const printSucces = (msg) => {
-  console.log(bgGreen(" SUCCES ") + " " + msg);
+  console.log(`${chalk.bgGreen(" SUCCES ")}  ${msg}`);
 };
 
-export { printError, printSucces };
+const printHelp = () => {
+  console.log(
+    dedent`${chalk.bgCyan(" HELP ")} 
+		 Without parametrs - print the weather
+		 -s [CITY] for installation a city
+		 -h for print help
+		 -t [API KEY] save token
+		 `
+  );
+};
+
+export { printError, printSucces, printHelp };
