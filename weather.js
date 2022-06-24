eaa8095b06c214097defed4c69176d12008e76d9
Vadim-Maul <v.v.maul681@gmail.com>
2022-06-24 08:45:09 +0200

    add storage service

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp, printError, printSucces } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
 
 const initCLI = () => {
   const args = getArgs(process.argv);
@@ -8,6 +9,9 @@ const initCLI = () => {
     printHelp();
   }
   if (args.s) {
+  }
+  if (args.t) {
+    saveKeyValue("token", args.t);
   }
 };
 
